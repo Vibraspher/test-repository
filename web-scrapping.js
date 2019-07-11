0166d08894cc772521e741151e8b53a97ef7db65
Biscoitta <allkes@outlook.com>
2019-07-11 04:19:07 -0300

    primeiro arquivo

---
+++ web-scrapping.js
@@ -0,0 +1,18 @@
+const request = require('request')
+const cheerio = require('cheerio')
+
+request('http://www.google.com', function (error, response, body) {
+
+	if (error) throw new Error(error)
+
+		const $ = cheerio.load(body)
+
+	$('.main-section div').each(function() {
+		
+		var post = $(this).find('.entry-title a')
+
+		console.log(`Postagem: ${nome}`)
+
+	})
+
+});
